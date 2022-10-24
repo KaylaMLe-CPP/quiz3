923f8f150e1c0ae174c3dd8b610c2cc93d39a588
Kayla Marie Le <kaylale2014@gmail.com>
2022-10-23 22:27:44 -0700

    figured out how to work with the input stream in C

--- q3/guess.c
+++ q3/guess.c
@@ -1,11 +1,60 @@
-// print message
-// save choice
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-// generate random number
-//// ask for guess
-//// check and reprompt or exit (success)
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    int choice = 0;
+    int max = 10;
 
-// change max
-// back to menu
+    while (choice != 3)
+    {
+        printf("\nPress 1 to play a game\nPress 2 to change the max number\nPress 3 to quit\n");
+        scanf("%d", &choice);
+        if (choice < 1 || choice > 3)
+        {
+            printf("\nInvalid choice\n");
+            continue;
+        }
 
-// thank and leave
+        if (choice == 1)
+        {
+            int target = (rand() % max) + 1;
+
+            int guess;
+            printf("\nGuess a number between 1 and %d or enter 'q' to end the game: ", max);
+            int scanSuccess = scanf("%d", &guess);
+
+            bool quit = false;
+            while (guess != target)
+            {
+                if (scanSuccess != 1)
+                {
+                    quit = true;
+                    break;
+                }
+
+                char *diff = target < guess ? "high" : "low";
+                printf("Your guess was too %s. Try again!: ", diff);
+                scanSuccess = scanf("%d", &guess);
+            }
+
+            if (!quit)
+            {
+                printf("\nThat's the right number!\n");
+            }
+        }
+        else if (choice == 2)
+        {
+            printf("\nEnter new max number: ");
+            scanf("%d", &max);
+        }
+        while ((getchar()) != '\n')
+            ;
+    }
+
+    printf("\nThanks for playing!\n");
+    return 0;
+}
