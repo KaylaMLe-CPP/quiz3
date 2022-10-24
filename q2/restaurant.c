c1606e124970eae52064fe60e4959aa47c0d033f
Kayla Marie Le <kaylale2014@gmail.com>
2022-10-23 21:40:46 -0700

    fixing git history to include q2 answer

--- q2/restaurant.c
+++ q2/restaurant.c
@@ -1,10 +1,33 @@
-// get args
-// validate (quantity?)
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-// enum/map items:price
-// srandomly choose one
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Incorrect number of arguments.\n");
 
-// tax = tax arg * price
-// total = tax + price + tip
+        return 1;
+    }
 
-// print
+    // the first argument passed is the tax
+    double tax = atof(argv[1]);
+    // the second argument passed is the tip
+    double tip = atof(argv[2]);
+
+    double menuPrices[4] = {9.95, 4.55, 13.25, 22.35};
+    char *menuItems[4] = {"Salad", "Soup", "Sandwich", "Pizza"};
+    srand(time(NULL));
+    int item = rand() % 4;
+
+    printf("\nSubtotal:\n%s\t$%.2f\n\n", menuItems[item], menuPrices[item]);
+
+    double taxAmt = menuPrices[item] * tax;
+    printf("Tax (%%%.2f): $%.2f\nTip: $%.2f\n", (tax * 100), taxAmt, tip);
+
+    double total = menuPrices[item] + taxAmt + tip;
+    printf("Total: $%.2f\n", total);
+
+    return 0;
+}
